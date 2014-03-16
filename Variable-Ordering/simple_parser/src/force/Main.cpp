6e92a953793c944acc80e1274fd60fb00d606e60
oPinon <olivierpinon@orange.fr>
2014-03-16 20:02:46 +0100

    Moved files to avoid have only one version of them

---
+++ Variable-Ordering/simple_parser/src/force/Main.cpp
@@ -0,0 +1,11 @@
+#include "Variable.h"
+#include "HyperEdge.h"
+#include "RandomGenerator.h"
+#include <iostream>
+
+#include "Tests.h"
+
+int main( int argc, const char* argv[] )
+{
+	spanEvolutionTest();
+}
